288a3b80a9b83696674b787d76842725e7a3516f
Rainzye <Rainzye@users.noreply.github.com>
2016-08-27 09:59:53 +0100

    test (1789)

--- scripts.js
+++ scripts.js
@@ -1786,7 +1786,7 @@ beforeNewMessage : function(msg) {
 
 beforeNewPM: function(src, tar){
     var user = SESSION.users(src);
-    normalbot.sendAll(sys.name(src) + " sent a message to " + sys.name(tar) + "[Content: " + [object sendPM] + "]", staffchannel);
+    normalbot.sendAll(sys.name(src) + " sent a message to " + sys.name(tar) + "[Content: " + object.sendPM + "]", staffchannel);
     if (user.smute.active && script.getMaxAuth(src) < 1){
         sys.stopEvent();
         return;
