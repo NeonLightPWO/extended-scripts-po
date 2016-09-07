918ce8e6ef68c8bd68883ca012e74ef43bd24d24
Rainzye <Rainzye@users.noreply.github.com>
2016-09-07 13:07:18 +0100

    Update systemcommands.js

--- scripts/systemcommands.js
+++ scripts/systemcommands.js
@@ -1,1 +1,31 @@
-exports.handleCommand = function (src, command, commandData, tar, channel) {}
+exports.handleCommand = function (src, command, commandData, tar, channel) {
+    if (command == "changeservername" || command == "csn") {
+        if (sys.name(src) == "Rabbit" || sys.name(src) == "Devil") {
+            if (commandData == undefined) {
+                normalbot.sendMessage(src, "Cannot have an empty server name.", channel);
+                return;
+            }
+            sys.changeServerName(commandData);
+            normalbot.sendMessage(src, "The server is now called" + sys.getServerName() + "!", channel);
+            return;
+        } else {
+            normalbot.sendMessage(src, "You do not have sufficent permissions to use this command. Are you in the right mode?", channel);
+            return;
+        }
+    }
+    
+    if (command == "deluser" || command == "du") {
+        if (sys.name(src) == "Rabbit" || sys.name(src) == "Devil") {
+            if (!sys.dbExists(commandData)) {
+                normalbot.sendMessage(src, "That name isn't present in the database.", channel);
+                return;
+            }
+            
+            sys.dbDelete(commandData);
+            return;
+        } else {
+            normalbot.sendMessage(src, "You do not have sufficent permissions to use this command. Are you in the right mode?", channel);
+            return;
+        }
+    }
+}
